454ee7a6213bdec86e8aad466f5c3169c1c11cdd
moonee <uuuu118@naver.com>
2020-06-13 01:42:11 +0900

    bfs중첩재귀수정필요

--- graph.cpp
+++ graph.cpp
@@ -85,10 +85,11 @@ string graph::record_trace(int start_x, int start_y, int nx, int ny) {
 	answer.append(wayTrace[find_x][find_y].dir, 0, 1);
 	cout << answer << endl;
 	while (true){
-		if (find_x == start_x && find_y == start_y) break;
+		if ((find_x == start_x && find_y == start_y)) break;
 		find_x = wayTrace[find_x][find_y].parent_x;
 		find_y = wayTrace[find_x][find_y].parent_y;
-		answer.append(wayTrace[find_x][find_y].dir,0,1);
+		if (find_x >= x || find_x < 0 || find_y >= y || find_y < 0) break;
+		answer.append(wayTrace[find_x][find_y].dir);
 		cout << wayTrace[find_x][find_y].dir << endl;
 	} 
 
@@ -115,10 +116,9 @@ bool graph::bfs(string& answer,int start_x, int start_y, int* res_x, int* res_y,
 		if (adjGraph[nx][ny].compare(goal)==0) { //goal 에 도착했다면 
 			(*res_x) = nx; //goal 위치 저장 
 			(*res_y) = ny;
-			if (goal.compare("F") == 0) { // goal이 F라면 ( 여기서 F는 우회해서 B를 밀었을 경우임)
-				answer.append(record_trace(start_x, start_y, nx, ny)); //F까지의 trace 추가해놓기
-				return true; }
 			if (goal.compare("T") == 0) {  //goal이 T라면 
+				(*res_x) = wayTrace[nx][ny].parent_x;
+				(*res_y) = wayTrace[nx][ny].parent_y;
 				answer.append(record_trace(start_x,start_y,nx,ny)); //T까지의 trace 추가해놓기 
 				return true; }
 			int x3=-1;
@@ -126,22 +126,28 @@ bool graph::bfs(string& answer,int start_x, int start_y, int* res_x, int* res_y,
 			bool flag=false;
 			if (goal.compare("B")==0) { //goal이 B라면 
 				answer=record_trace(start_x, start_y, nx, ny); 
-				 // B직전 까지의 trace 저장하기 
-				flag = bfs(answer,nx, ny, &x3, &y3, "T"); // T 찾았는지 ?  찾았다면 True 못찾았다면 False 
+				 // B직전 까지의 trace 저장하기
+				int px = wayTrace[nx][ny].parent_x;
+				int py = wayTrace[nx][ny].parent_y;
+				flag = bfs(answer,px, py, &x3, &y3, "T"); // T 찾았는지 ?  찾았다면 True 못찾았다면 False 
 				if (is_corner(nx, ny) == false && flag && (x3 != -1 && y3 != -1)) { //코너가 아니고, T를 찾았다면 
 					(*res_x) = x3; //T 찾은 위치 저장후 
 					(*res_y) = y3;
 					return true; //반환 
 				}
 				else if(flag==false || x3==-1 || y3==-1) return false; // T를 못찾았다면 false 
-				adjGraph[x3][y3] = "F"; //그외.. 코너에 몰린 경우 T자리를 F로 바꾸고, B자리를 '.'으로 바꾸어준다.
-				goal = "F";
-				adjGraph[nx][ny] = ".";
+				adjGraph[x3][y3] = "B"; //그외.. 코너에 몰린 경우 T의 바로 앞자리를 F로 바꾸고, B자리를 '.'으로 바꾸어준다.
+				adjGraph[nx][ny] = "#";
 				start_x = wayTrace[nx][ny].parent_x; //start지점과 nx ny 자리도 바꾸어준다. 박스를 민 자리로.. 
-				start_y = wayTrace[nx][ny].parent_y;
-	
+				start_y = wayTrace[nx][ny].parent_y; //박스를 민 자리는..가지 않는다 .
 				nx = start_x;
 				ny = start_y;
+				
+				for (int i = 0; i < x; i++) {
+					for (int j = 0; j < y; j++) {
+						visited[i][j] = false;
+					}
+				}
 			}
 		}
 		if (nx != 0 && adjGraph[nx - 1][ny].compare("#") != 0 && visited[nx - 1][ny] != true) {
@@ -185,6 +191,7 @@ bool graph::bfs(string& answer,int start_x, int start_y, int* res_x, int* res_y,
 			queue.add(s);
 			visited[nx][ny+1] = true;
 		}
+		adjGraph[nx][ny] = ".";
 	}
 	return false;
 }
